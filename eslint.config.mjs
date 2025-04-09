1db9915ae3e0bbc4f64e294d057cb52bde02932d
jerabera <jera@berachain.com>
2025-04-09 16:31:02 -0400

    add import sorting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default defineConfig([
   globalIgnores([".dist/", "package-lock.json", "package.json"]),
@@ -12,8 +13,11 @@ export default defineConfig([
   tseslint.configs.recommended,
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  { plugins: { "simple-import-sort": simpleImportSort } },
   {
     rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
@@ -29,3 +33,5 @@ export default defineConfig([
     },
   },
 ]);
+
+
