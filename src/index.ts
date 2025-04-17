71a5f62fe3b6f735c857a49c64349ade7019e43e
Bill Bodell <bill@dkoda.xyz>
2025-04-17 13:06:35 -0400

    Add Wasabi Vault Adapter (#12)
    * Add Wasabi sWBERA Vault Adapter
    
    * Rename and add sHONEY vault
    
    * Fix class name, export in index
    
    ---------
    
    Signed-off-by: Bill Bodell <bill@dkoda.xyz>

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { SNECTVaultAdapter } from "./vaults/beraborrow/sNECTVaultAdapter";
+export { WasabiVaultAdapter } from "./vaults/wasabi/wasabiVaultAdapter";
 export { AquaBeraAdapter } from "./vaults/aquabera/vaultAdapter";
