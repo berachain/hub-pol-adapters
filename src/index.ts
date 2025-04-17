3ae22464365af26dc23156ceeb2888a52689d9f4
roomtin <61144046+roomtin@users.noreply.github.com>
2025-04-17 14:54:08 -0400

    Add Aquabera Henlo Adapter (#16)
    * Add aquabera adapter
    
    * change incentive tokens to wbera and beramo
    
    * export aquabera and add chain ID to token type
    
    * remove incentiveToken function that's never used
    
    * remove just the unused argument in incentiveToken()
    
    * Update Beramo adapter with specific name, add Henlo adapter
    
    ---------
    
    Signed-off-by: roomtin <61144046+roomtin@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { SNECTVaultAdapter } from "./vaults/beraborrow/sNECTVaultAdapter";
+export { AquaBeraBeramoAdapter } from "./vaults/aquabera/beramoWberaVaultAdapter";
+export { AquaBeraHenloAdapter } from "./vaults/aquabera/henloWberaVaultAdapter";
 export { WasabiVaultAdapter } from "./vaults/wasabi/wasabiVaultAdapter";
-export { AquaBeraAdapter } from "./vaults/aquabera/vaultAdapter";
