fc7055dd1125e8c2bac157e69a4478e935439698
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2025-05-05 17:47:17 -0300

    feat(berachain): consolidate 14 adaptors into one

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,7 @@ import { AquaBeraBeramoAdapter } from "./vaults/aquabera/beramoWberaVaultAdapter
 import { AquaBeraHenloAdapter } from "./vaults/aquabera/henloWberaVaultAdapter";
 import { WasabiVaultAdapter } from "./vaults/wasabi/wasabiVaultAdapter";
 import { DRUSDVaultAdapter } from "./vaults/dolomite/drusdVaultAdapter";
-import { CtBeraUSDT0VaultAdapter } from "./vaults/berachain/ctBeraUSDT0VaultAdapter";
-import { CtBeraUSDeVaultAdapter } from "./vaults/berachain/ctBeraUSDeVaultAdapter";
-import { CtBeraUSDCeVaultAdapter } from "./vaults/berachain/ctBeraUSDCeVaultAdapter";
-import { CtBeraHONEYVaultAdapter } from "./vaults/berachain/ctBeraHONEYVaultAdapter";
-import { CtBerasUSDeVaultAdapter } from "./vaults/berachain/ctBerasUSDeVaultAdapter";
-import { CtBeraWETHVaultAdapter } from "./vaults/berachain/ctBeraWETHVaultAdapter";
-import { CtBeraETHVaultAdapter } from "./vaults/berachain/ctBeraETHVaultAdapter";
-import { CtBeraLBTCVaultAdapter } from "./vaults/berachain/ctBeraLBTCVaultAdapter";
-import { CtBeraWBTCVaultAdapter } from "./vaults/berachain/ctBeraWBTCVaultAdapter";
-import { CtBerauniBTCVaultAdapter } from "./vaults/berachain/ctBerauniBTCVaultAdapter";
-import { CtBeraFBTCVaultAdapter } from "./vaults/berachain/ctBeraFBTCVaultAdapter";
-import { CtBeraWBERAVaultAdapter } from "./vaults/berachain/ctBeraWBERAVaultAdapter";
-import { CtBeraBaddiesHONEYVaultAdapter } from "./vaults/berachain/ctBeraBaddiesHONEYVaultAdapter";
-import { CtBeraBaddiesWBERAVaultAdapter } from "./vaults/berachain/ctBeraBaddiesWBERAVaultAdapter";
+import { BerachainVaultAdapter } from "./vaults/berachain/berachainVaultAdapter";
 
 export {
     SNECTVaultAdapter,
@@ -24,18 +11,5 @@ export {
     AquaBeraHenloAdapter,
     WasabiVaultAdapter,
     DRUSDVaultAdapter,
-    CtBeraUSDT0VaultAdapter,
-    CtBeraUSDeVaultAdapter,
-    CtBeraUSDCeVaultAdapter,
-    CtBeraHONEYVaultAdapter,
-    CtBerasUSDeVaultAdapter,
-    CtBeraWETHVaultAdapter,
-    CtBeraETHVaultAdapter,
-    CtBeraLBTCVaultAdapter,
-    CtBeraWBTCVaultAdapter,
-    CtBerauniBTCVaultAdapter,
-    CtBeraFBTCVaultAdapter,
-    CtBeraWBERAVaultAdapter,
-    CtBeraBaddiesHONEYVaultAdapter,
-    CtBeraBaddiesWBERAVaultAdapter,
+    BerachainVaultAdapter,
 };
