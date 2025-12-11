ed4adbd76edb254646dea5371239346f995baf97
bearpong <pong@berachain.com>
2025-12-11 20:37:27 +0100

    fix: runtime compatibility and imports

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  bundle: true,  
+  format: ['esm', 'cjs'],
+  platform: 'node',
+  target: 'node20',
+  noExternal: [
+    'graphql-request',
+  ],
+  skipNodeModulesBundle: false,  
+})
