0c6f53c0e86e159447eed8e9c85dcf3ade5d642f
Pong <pong@berachain.com>
2026-07-10 14:56:56 +0100

    chore: remove adapters covered by berachain api (#77)

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
-import { AquaBeraAdapter } from "./vaults/aquabera/AquaBeraAdapter";
-
-import { ConcreteVaultAdapter } from "./vaults/concrete/concreteVaultsAdapter";
 import { SxVaultAdapter } from "./vaults/sx/sxVaultAdapter";
 import { BullIshGaugeAdapter } from "./vaults/bullish/BullIshGaugeAdapter";
 import { IVXVaultAdapter } from "./vaults/ivx/ivx-adapter";
 import { SolvBTCBeraVaultAdapter } from "./vaults/solv/SolvBTCBeraVaultAdapter";
 
 import { BaseAdapter } from "./types";
-import { BrownFiVaultAdapter } from "./vaults/brownfi/BrownFiVaultAdapter";
 
 export const adapters = [
-    AquaBeraAdapter,
-    ConcreteVaultAdapter,
     SxVaultAdapter,
     BullIshGaugeAdapter,
     IVXVaultAdapter,
     SolvBTCBeraVaultAdapter,
-    BrownFiVaultAdapter,
 ] as const satisfies (typeof BaseAdapter)[];
